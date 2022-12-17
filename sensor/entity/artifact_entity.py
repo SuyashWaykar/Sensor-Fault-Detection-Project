589b398009a889e40426730f409424a816f27de3
SuyashWaykar <suyashwaykar1299@gmail.com>
2022-12-17 19:39:32 +0530

    completed data transformation stage

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -10,8 +10,14 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     report_file_path:str
 
+@dataclass
+class DataTransformationArtifact:
+    transform_object_path:str
+    transform_train_path:str
+    transform_test_path:str
 
-class DataTransformationArtifact:...
+
+
 class ModelTrainerArtifact:...
 class ModelEvaluationArtifact:...
 class ModelPusherArtifact:...
